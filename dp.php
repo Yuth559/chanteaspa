bbdd41cbd30b32b948eb0b98cadf8351ec04ae39
Yuth559 <151505320+Yuth559@users.noreply.github.com>
2023-11-20 14:26:15 +0700

    Add files via upload

---
+++ dp.php
@@ -0,0 +1,12 @@
+<?php
+$_SERVER = "sql213.infinityfree.com";
+$username = "	if0_35445384";
+$password = "AuwpuwCOov9IaZ";
+$dbname ="if0_35445384_yuthzz59	";
+
+$com = mysqli_connect($server, $username, $password, $password);
+
+if(!$conn){
+    die("Connection Failed:".mysqli_connect_error());
+}
+?>
